1e605ce0a70ee11ea2b9315f1891a640714ade17
Hadi Mohammadi <hadi@brandeis.edu>
2026-06-29 15:14:10 -0400

    Edit cors typo

--- studyhub-express/server.js
+++ studyhub-express/server.js
@@ -10,7 +10,7 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.use(corse({origin: process.env.CLIENT_URL || "http://localhost:5173", credentials: true}))
+app.use(cors({origin: process.env.CLIENT_URL || "http://localhost:5173", credentials: true}))
 app.use(express.json())
 
 app.use('/api/notes', notesRouter);
